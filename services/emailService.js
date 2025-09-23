ffd5136a231433e76cead369eca942f85258fa87
Oumar Diane <oumardiane399@gmail.com>
2025-09-23 13:15:07 +0200

    Projet clean sans secrets6

--- services/emailService.js
+++ services/emailService.js
@@ -7,7 +7,7 @@ const createTransporter = () => {
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 };
