8d1a92250a5d692365e6b1c22d98ee780a62777f
Oumar Diane <oumardiane399@gmail.com>
2026-02-01 19:54:51 +0100

    hjdb1

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ import Prevan from './Gestion/prevann.js';
 import Commande from './Gestion/commande.js';
 import Annalyse from './Gestion/Annalyse-financiere.js';
 import Analysecren from './Gestion/Analyse-cren.js';
-import Analyseabo from './Gestion/Analyseabo.js';
+import Analyseab from './Gestion/Analyseabo.js';
 
 
 
@@ -70,7 +70,7 @@ app.use('/api/prevannule', Prevan);
 app.use('/api/commande', Commande);
 app.use('/api/annalyse', Annalyse);
 app.use('/api/annalyse-creneaux', Analysecren);
-app.use('/api/annalyse-abonnes', Analyseabo);
+app.use('/api/annalyse-Abonner', Analyseab);
 
 
 // 🏥 Health check endpoint amélioré
